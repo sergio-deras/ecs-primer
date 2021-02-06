acbd74a28fbd297755f66e7d9cc39c57c1523eec
Sergio Deras <sergio-deras@github.com>
2021-02-06 11:35:21 -0600

    Add env in an advanced sample

--- ecs_primer/ecs_primer_stack_adv.py
+++ ecs_primer/ecs_primer_stack_adv.py
@@ -1,10 +1,12 @@
+from typing import Mapping
+
 from aws_cdk import core
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_ecs as ecs
 import aws_cdk.aws_ecs_patterns as ecs_patterns
 
 
-class EcsPrimerStack(core.Stack):
+class EcsPrimerStackAdv(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -19,10 +21,14 @@ class EcsPrimerStack(core.Stack):
                 "MyFargateApp",
                 cluster=myCluster,
                 listener_port=80,  # the default container port is port 80
+                memory_limit_mib=1024,
                 task_image_options=
                 ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                    image=ecs.ContainerImage.from_asset("./app/")
+                    image=ecs.ContainerImage.from_asset("./app/"),
+                    environment=dict(ENV_VAR1="test environment variable 1 value",
+                                     ENV_VAR2="test environment variable 1 value")
                 )
+
             )
         # cdk bootstrap
 
